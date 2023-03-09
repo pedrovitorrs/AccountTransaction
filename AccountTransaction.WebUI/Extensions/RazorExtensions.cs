173a673c1e94f5cbb7c8b981eaba800a08a4dfbf
pedrovitorrs <pedro442.rodrigues@gmail.com>
2023-03-08 21:49:05 -0300

    feat: inserindo listagem de cartao.

---
+++ AccountTransaction.WebUI/Extensions/RazorExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace AccountTransaction.WebUI.Extensions
+{
+    public static class RazorExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="valor"></param>
+        /// <returns></returns>
+        public static string FormatMoney(this RazorPage page, decimal valor)
+        {
+            return FormatMoney(valor);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="valor"></param>
+        /// <returns></returns>
+        private static string FormatMoney(decimal valor)
+        {
+            return string.Format(System.Globalization.CultureInfo.GetCultureInfo("pt-BR"), "{0:C}", valor);
+        }
+    }
+}
